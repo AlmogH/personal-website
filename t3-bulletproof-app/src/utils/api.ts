b9afb69ddbe0cb6900190bb2a550d42b2962aa45
AlmogH <almoghagbi96@gmail.com>
2025-08-20 18:38:08 +0000

    chore: remove empty code changes

--- t3-bulletproof-app/src/utils/api.ts
+++
@@ -1,9 +0,0 @@
-import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
-
-import { type AppRouter } from "~/server/api/root";
-
-export { createTRPCReact, type CreateTRPCReact } from "@trpc/react-query";
-
-export type RouterInputs = inferRouterInputs<AppRouter>;
-
-export type RouterOutputs = inferRouterOutputs<AppRouter>;
